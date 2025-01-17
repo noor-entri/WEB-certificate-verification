5c5cc2ffd732687a635aaad03e3da7439ecd5997
Noor Fabid <noor.fabid@entri.me>
2025-01-17 17:42:28 +0530

    Add custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://noor-entri.github.io',
-  base: '/WEB-certificate-verification',
+  site: 'https://dev-certificate.entri.team',
   env: {
     schema: {
       API_BASE_URL: envField.string({ context: "client", access: "public", default: "https://channel7.dev.entri.app/api", url: true }),
